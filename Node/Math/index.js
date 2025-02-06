3c342af51b62686f354b19d2152e526166614cd1
Balaji <balu17.marripally@gmail.com>
2025-02-06 10:15:35 +0530

    added

---
+++ Node/Math/index.js
@@ -0,0 +1,3 @@
+import {add,sub} from "./math.js";
+console.log(add(10,15));
+console.log(sub(10,15));
---
+++ Node/Math/math.js
@@ -0,0 +1,7 @@
+const add = (a,b)=>{
+    return a+b ;
+}
+const sub = (a,b)=>{
+    return b-a;
+}
+ export{add,sub};
